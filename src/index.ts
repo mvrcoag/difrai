542074de50d636de8dba8daa6538942a32162a2c
Marco A. García <jag-crocks-3v@icloud.com>
2026-01-29 22:09:35 -0600

    :feat logger and change hello message

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import 'dotenv/config'
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import webhookRoute from './modules/code-review/routes/webhook-route.js'
-import { env } from './env.js'
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import webhookRoute from "./modules/code-review/routes/webhook-route.js";
+import { env } from "./env.js";
+import { logger } from "hono/logger";
 
-const app = new Hono()
+const app = new Hono();
 
-app.route('/webhooks', webhookRoute)
+app.use(logger());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("/webhooks", webhookRoute);
 
-serve({
-  fetch: app.fetch,
-  port: env.PORT, 
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Hello Difrai");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: env.PORT,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
